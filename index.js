79528397e36b49b93ae0fb46cb548f233fa821b0
JoeBright1619 <brightcollin1619@gmail.com>
2024-05-22 19:27:37 +0200

    feat: final redit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const multer = require('multer');
 var cors = require('cors');
 require('dotenv').config()
 
@@ -11,7 +12,23 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+var upload = multer({dest:'uploads/'})
 
+app.post('/api/fileanalyse',upload.single('upfile') ,async(req, res)=>{
+
+  var file = req.file
+  try{
+    console.log(file)
+    res.json({
+      name: file.originalname,
+      type: file.mimetype,
+      size: file.size
+    })
+  }
+  catch(err){
+    res.sendStatus(err)
+  }
+});
 
 
 const port = process.env.PORT || 3000;
